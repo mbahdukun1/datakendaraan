e29f882a666630b72d878e9189d884d4f13be3e8
mbahdukun1 <dicmaulana09@gmail.com>
2022-12-01 21:26:08 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// function data(no, noregis, name, merk, tahun, kapasitas, warna, bahan, action) {
+//   this.no = no;
+//   this.noregis = noregis;
+//   this.name = name;
+//   this.merk = merk;
+//   this.tahun = tahun;
+//   this.kapasitas = kapasitas;
+//   this.warna = warna;
+//   this.bahan = bahan;
+//   this._action = action;
+
+//   this.action = function () {
+//     return this._action;
+//   };
+// }
+
+// // $("#example").DataTable({
+// //   data: [new data("1", "B-7763-TXY", "Lionel Messi", "Honda PCX", "2018", "150 cc", "Hitam", "Bensin", "Button")],
+// // });
+// function Employee(name, position, salary, office) {
+//   this.name = name;
+//   this.position = position;
+//   this.salary = salary;
+//   this._office = office;
+
+//   this.office = function () {
+//     return this._office;
+//   };
+// }
+
+// $("#example").DataTable({
+//   data: [new Employee("Tiger Nixon", "System Architect", "$3,120", "Edinburgh"), new Employee("Garrett Winters", "Director", "$5,300", "Edinburgh")],
+//   columns: [{ data: "name" }, { data: "salary" }, { data: "office" }, { data: "position" }],
+// });
